f7c6959a2084e0b8ed80cc1df18e0de5677ecd18
Zan Pecovnik <zan.pecovnik@sinergise.com>
2022-07-01 11:26:17 +0200

    Add logo to footer and change background

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import logo from "../../assets/MV Logo1.png";
+
 import "./Footer.scss";
 
 const Footer = () => {
@@ -9,7 +11,9 @@ const Footer = () => {
         <i className="fa-solid fa-at" />
         <div className="footer-left-text">matej.verbuc@gmail.com</div>
       </div>
-      <div className="footer-logo">LOGO</div>
+      <div className="footer-logo">
+        <img alt="logo" src={logo} />
+      </div>
       <div className="footer-right">
         <i className="fa-solid fa-phone" />
         <div className="footer-left-text">070-388-348</div>
